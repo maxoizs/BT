ceb36ed40ef39e3201a0d619abd38a137eedd0fd
maxoizs <abdalaal.mohamed@gmail.com>
2018-10-02 04:45:34 +0100

    add unit tests

--- BS.UnitTest/BoardTests.cs
+++ BS.UnitTest/BoardTests.cs
@@ -63,6 +63,45 @@ namespace Tests
             var added = board.AddShip(new Destroyer(), new Coordinates(14, 13), Direction.Down);
 
             Assert.False(added);
-        }        
+        }
+
+        [Test]
+        public void GivenInValidHit_BoardShouldIncreaseMisses()
+        {
+            var board = new Board(_userInput);
+            var added = board.AddShip(new Destroyer(), new Coordinates(1, 1), Direction.Down);
+
+            board.TakeHit(new Coordinates(3, 3));
+
+            Assert.That(board.Misses, Is.EqualTo(1));
+            Assert.That(board.Hits, Is.EqualTo(0));
+        }
+
+
+        [Test]
+        public void GivenValidHit_BoardShouldIncreaseHits()
+        {
+            var board = new Board(_userInput);
+            var added = board.AddShip(new Destroyer(), new Coordinates(1, 1), Direction.Down);
+
+            board.TakeHit(new Coordinates(1, 1));
+
+            Assert.That(board.Misses, Is.EqualTo(0));
+            Assert.That(board.Hits, Is.EqualTo(1));
+        }
+
+        
+        [Test]
+        public void GivenValidHitsEqualToShip_BoardLive_ShouldBeFalse()
+        {
+            var board = new Board(_userInput);
+            var added = board.AddShip(new Destroyer(), new Coordinates(1, 1), Direction.Down);
+
+            board.TakeHit(new Coordinates(1, 1));
+            board.TakeHit(new Coordinates(1, 2));
+
+            Assert.That(board.Hits, Is.EqualTo(2));
+            Assert.IsFalse(board.IsLive());
+        }
     }
 }
